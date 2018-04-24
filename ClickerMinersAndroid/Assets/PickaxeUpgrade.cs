dfcc8599479cb75c0d6100b36b7e29bf44b49d02
freddiezeb <Freddiezeb@hotmail.com>
2018-04-24 21:17:19 +0200

    Added an Upgrade Pickaxe button with some basic functionality

---
+++ ClickerMinersAndroid/Assets/PickaxeUpgrade.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickaxeUpgrade : MonoBehaviour
+{
+
+    public int upgradeCost;
+    public int upgradeMultiplier;
+    public int pickaxeLevel;
+    public Text levelDisplay;
+    public Text costDisplay;
+
+    // Use this for initialization
+    void Start()
+    {
+        //pickaxeLevel = globalStats.pickaxeLevel;
+        costDisplay.text = upgradeCost.ToString();
+        if (pickaxeLevel > 1)
+        {
+            for (int i = 1; i < pickaxeLevel; i++)
+            {
+                upgradeCost += (pickaxeLevel * upgradeMultiplier);
+            }
+        }
+    }
+
+    public void UpgradePickaxe()
+    {
+        if (GlobalClicks.currencyCount >= upgradeCost)
+        {
+            pickaxeLevel++;
+            levelDisplay.text = pickaxeLevel.ToString();
+            GlobalClicks.currencyCount -= upgradeCost;
+            UpgradeCostIncrease();
+            costDisplay.text = upgradeCost.ToString();
+        }
+    }
+
+    private void UpgradeCostIncrease()
+    {
+        upgradeCost += (pickaxeLevel * upgradeMultiplier);
+    }
+}
--- ClickerMinersAndroid/Assets/Scripts/BuyMine.cs
+++ ClickerMinersAndroid/Assets/Scripts/BuyMine.cs
@@ -13,44 +13,99 @@ public class BuyMine : MonoBehaviour
     public int obsidianMineCost;
     public int goldMineCost;
 
-    public void MinePayment(int mineCost)
-    {
-        if (GlobalClicks.currencyCount >= mineCost)
-        {
-            GlobalClicks.currencyCount -= mineCost;
-        }
-        else
-        {
-            StartCoroutine(FadeTextToZeroAlpha(2f, textBox));
-        }
-    }
+	Mine gravelMine;
+	Mine graniteMine;
+	Mine metalMine;
+	Mine obsidianMine;
+	Mine goldMine;
+
+	List<Mine> mineList;
+
+	void Start ()
+	{
+		gravelMine = new Mine ("gravelMine", gravelMineCost);
+		graniteMine = new Mine ("graniteMine", graniteMineCost);
+		metalMine = new Mine ("metalMine", metalMineCost);
+		obsidianMine = new Mine ("obsidianMine", obsidianMineCost);
+		goldMine = new Mine ("goldMine", goldMineCost);
+
+		mineList = new List<Mine>();
+		mineList.Add (gravelMine);
+		mineList.Add (graniteMine);
+		mineList.Add (metalMine);
+		mineList.Add (obsidianMine);
+		mineList.Add (goldMine);
+	}
 
     public void BuyGravelMine()
     {
-        MinePayment(gravelMineCost);
+		HandelMine(gravelMine);
     }
 
     public void BuyGraniteMine()
     {
-        MinePayment(graniteMineCost);
+		HandelMine(graniteMine);
     }
 
     public void BuyMetalMine()
     {
-        MinePayment(metalMineCost);
+		HandelMine(metalMine);
     }
 
     public void BuyObsidianMine()
     {
-        MinePayment(obsidianMineCost);
+		HandelMine(obsidianMine);
     }
 
     public void BuyGoldMine()
     {
-        MinePayment(goldMineCost);
+		HandelMine(goldMine);
     }
 
+	public void MinePayment(Mine mine)
+	{
+		if (GlobalClicks.currencyCount >= mine.Cost)
+		{
+			GlobalClicks.currencyCount -= mine.Cost;
+			mine.Unlocked = true;
+			mine.Active = true;
+		}
+		else
+		{
+			StartCoroutine(FadeTextToZeroAlpha(2f, textBox));
+		}
+	}
 
+	void HandelMine(Mine mine)
+	{
+		UnActivateMine (mine);
+		if (!mine.Unlocked) 
+		{
+			MinePayment (mine);
+		} 
+		else if(mine.Unlocked) 
+		{
+			if (mine.Active)
+			{
+				mine.Active = false;
+			} else 
+			{
+				mine.Active = true;
+			}
+		}
+		Debug.Log (mine.Name + " Active: " + mine.Active.ToString ());
+	}
+
+	void UnActivateMine(Mine currentMine)
+	{
+		bool isActive = currentMine.Active;
+		for (int i = 0; i < mineList.Count; i++) 
+		{
+			mineList [i].Active = false;
+		}
+		currentMine.Active = isActive;
+	}
+		
     public IEnumerator FadeTextToZeroAlpha(float t, Text i)
     {
         i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
@@ -60,4 +115,26 @@ public class BuyMine : MonoBehaviour
             yield return null;
         }
     }
+	public class Mine
+	{
+		string name;
+		int cost;
+		bool unlocked = false;
+		bool active = false;
+
+		public string Name { get { return name; } }
+
+		public int Cost { get { return cost; } }
+
+		public bool Unlocked { get { return unlocked; } set { unlocked = value; } }
+
+		public bool Active { get { return active; } set { active = value; } }
+
+		public Mine(string name, int cost)
+		{
+			this.name = name;
+			this.cost = cost;
+		}
+	}
 }
+
